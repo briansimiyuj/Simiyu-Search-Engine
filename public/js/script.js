ccedaa73bec490e50e78781bfe5f1411a53eba3c
Brian Simiyu <briansimiyuj@gmail.com>
2023-04-08 09:35:46 +0300

    Grabbing form element from DOM

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+document.addEventListener("readystatechange", (e) => {
+    initApp();
+});
+const initApp = () => {
+    const form = document.querySelector("form");
+    console.log(form);
+};
---
+++ src/script.ts
@@ -0,0 +1,14 @@
+document.addEventListener("readystatechange", (e) =>{
+
+    initApp()
+
+})
+
+
+const initApp = () =>{
+
+   const form = document.querySelector("form") as HTMLFormElement
+
+   console.log(form)
+
+}
